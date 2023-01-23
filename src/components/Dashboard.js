c15045785dd98e6129ce831f104411cdf22c7d73
Mjd Thif <mjd.thif@icloud.com>
2023-01-23 22:48:07 +0100

    adde the api route

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,11 +12,37 @@ export default function Dashboard() {
   const [isGenerating, setIsGenerating] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(jobDescription);
+    setIsCopied(true);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsGenerating(true);
+    const res = await fetch("/api/returnJobDescription", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        jobTitle,
+        industry,
+        keyWords,
+        tone,
+        numWords,
+      }),
+    });
+    setIsGenerating(false);
+    const data = await res.json();
+    setJobDescription(data.jobDescription.trim());
+  };
+
   return (
     <div className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="grid gap-y-12 md:grid-cols-2 md:gap-x-12 ">
         <div className="">
-          <form>
+          <form onSubmit={(e) => handleSubmit(e)}>
             <div className="flex flex-col">
               <label className="sr-only" htmlFor="jobTitle">
                 Job Title
@@ -117,18 +143,18 @@ export default function Dashboard() {
               rows={
                 jobDescription === ""
                   ? 7
-                  : jobDescription.split("\\n").length + 12
+                  : jobDescription.split("\n").length + 12
               }
               name="output"
-              onChange={(e) => setJobDescription(e.target.value)}
               value={jobDescription}
+              onChange={(e) => setJobDescription(e.target.value)}
               disabled={jobDescription === ""}
               id="output"
               placeholder="AI Generated Job Description"
               className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
             />
             <button
-              onClick={() => {}}
+              onClick={handleCopy}
               className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               type="submit"
               disabled={jobDescription === ""}
