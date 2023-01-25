24a4d1d00f002da3e7ad1c5acbed66f56a5a12c1
Mjd Thif <mjd.thif@icloud.com>
2023-01-25 22:51:40 +0100

    build

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>AI Job Description Generator</title>
+        <title>Description Generator</title>
         <meta name="description" content="AI Job Description Generator" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -17,13 +17,10 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center px-4 py-2">
           <h1 className="text-4xl md:text-6xl font-bold">
             AI Job Description Generator
-            <span className="text-4xl md:text-6xl font-bold text-blue-600">
-              .
-            </span>
           </h1>
           <p className="mt-3 text-2xl">
             Create Beautiful
-            <span className="text-2xl font-bold text-blue-600">
+            <span className="text-2xl font-bold text-red-600">
               Job Descriptions
             </span>
             in Seconds
